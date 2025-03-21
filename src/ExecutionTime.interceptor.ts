d0f9d011dcdaba9a62346bcc4f9c8ed51d13c2cb
Grafiel Aprilio Sitanggang <2381028@unai.edu>
2025-03-21 10:21:53 +0900

    handson 8

---
+++ src/ExecutionTime.interceptor.ts
@@ -0,0 +1,16 @@
+import { ExecutionContext, CallHandler, Logger, NestInterceptor } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+export class ExecutionTime implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        const handler = context.getHandler();
+        const methodName = handler.name;
+        const className = context.getClass().name;
+        this.logger.log(`Before... Calling ${className}.${methodName}`);
+        const StartTime = Date.now();
+        return next.handle().pipe(tap(() => {
+            this.logger.log(`After... ${className}.${methodName} took ${Date.now() - StartTime}ms`);
+        }));
+    }
+    private readonly logger = new Logger(ExecutionTime.name);
+}
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post, Delete, Get, Put, Param } from '@nestjs/common';
+import { Body, Controller, Post, Delete, Get, Put, Param, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { create } from 'domain';
 import { CreateSongDTO } from './create-song.dto';
 import { SongsService } from './songs.service';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { ExecutionTime } from 'src/ExecutionTime.interceptor';
 
 @Controller('songs')
 export class SongsController {
     constructor(private songService: SongsService) {}
 
     @Post()
-    create(@Body() createSongDTO: CreateSongDTO) {
+    @UseInterceptors(ExecutionTime)
+    create(@Body(new ValidationPipe) createSongDTO: CreateSongDTO) {
         return this.songService.create(createSongDTO);
     }
 
